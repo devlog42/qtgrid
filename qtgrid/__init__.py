3eee12dd5f31d4e05c77cfd6bf8b8492b86695ae
devlog42 <devlog@gmx.de>
2021-06-07 13:38:10 +0200

    Modify setup.py and module '__version__'.
    Remove 'extra_require' dev packages in 'setup.py'.
    Correct the '__version__' variable in 'qtgrid/__init__.py'.

--- qtgrid/__init__.py
+++ qtgrid/__init__.py
@@ -2,7 +2,7 @@
 The *qtgrid* package only contains the `qtgrid.qtgrid` module
 """
 
-__version__ = '0.3.0'
+__version__ = '1.0.0-beta'
 """Version"""
 
 from .qtgrid import Grid
--- setup.py
+++ setup.py
@@ -139,15 +139,7 @@ setup(
     # Similar to `install_requires` above, these must be valid existing
     # projects.
     extras_require={
-        "dev": [
-            "pytest",
-            "pydoctor",
-            "pydoctor[rst]",
-            "pipenv-setup",
-            "build",
-            "twine",
-            "mkdocs",
-        ]
+        "dev": []
     },  # Optional
     # If there are data files included in your packages that need to be
     # installed, specify them here.
