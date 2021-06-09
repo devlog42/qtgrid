fda9fefc0bcd82f46f69f7342d2706229bedf86d
devlog42 <devlog@gmx.de>
2021-06-09 12:00:23 +0200

    Note new version
    Due to a new PyPi upload, note the new version in
    
    - docs-source/mkapi.sh
    - qtgrid/__init__.py
    - setup.py
    - tests/test_gridhandle_00_version.py

--- qtgrid/__init__.py
+++ qtgrid/__init__.py
@@ -4,5 +4,5 @@ The *qtgrid* package only contains the `qtgrid.qtgrid` module
 
 from .qtgrid import Grid
 
-__version__ = '1.0.0-beta'
+__version__ = '1.0.0-beta.1'
 """Version"""
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.0.0-beta",  # Required
+    version="1.0.0-beta.1",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
@@ -141,9 +141,7 @@ setup(
     #
     # Similar to `install_requires` above, these must be valid existing
     # projects.
-    extras_require={
-        "dev": []
-    },  # Optional
+    extras_require={"dev": []},  # Optional
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     #
